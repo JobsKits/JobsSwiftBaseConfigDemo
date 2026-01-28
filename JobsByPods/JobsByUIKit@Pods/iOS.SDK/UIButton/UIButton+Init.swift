27bb8efe58d3b738b95ca8e069240eedb5fc3871
Jobs <lg295060456@gmail.com>
2026-01-28 13:41:23 +0700

    no message

--- JobsByPods/JobsByUIKit@Pods/iOS.SDK/UIButton/UIButton+Init.swift
+++ JobsByPods/JobsByUIKit@Pods/iOS.SDK/UIButton/UIButton+Init.swift
@@ -4,48 +4,49 @@
 //
 //  Created by Jobs on 12/3/25.
 //
+
 #if os(OSX)
 import AppKit
 #elseif os(iOS) || os(tvOS)
 import UIKit
 #endif
 // MARK: - init
-public extension UIButton {
-    convenience init(x: CGFloat,
-                     y: CGFloat,
-                     w: CGFloat,
-                     h: CGFloat,
-                     target: AnyObject,
-                     action: Selector) {
+extension UIButton {
+    public convenience init(x: CGFloat,
+                            y: CGFloat,
+                            w: CGFloat,
+                            h: CGFloat,
+                            target: AnyObject,
+                            action: Selector) {
         self.init(frame: CGRect(x: x, y: y, width: w, height: h))
         addTarget(target, action: action, for: .touchUpInside)
     }
     @available(iOS 7.0, *)
-    static func sys() -> UIButton {
-        UIButton(type: .system).byBackgroundColor(.clear)
+    public static func sys() -> UIButton {
+        UIButton(type: .system).byBackgroundColor(.clear, for: .normal)
     }
     @available(iOS 13.0, *)
-    static func close() -> UIButton {
-        UIButton(type: .close).byBackgroundColor(.clear)
+    public static func close() -> UIButton {
+        UIButton(type: .close).byBackgroundColor(.clear, for: .normal)
     }
 
-    static func custom() -> UIButton {
-        UIButton(type: .custom).byBackgroundColor(.clear)
+    public static func custom() -> UIButton {
+        UIButton(type: .custom).byBackgroundColor(.clear, for: .normal)
     }
 
-    static func detailDisclosure() -> UIButton {
-        UIButton(type: .detailDisclosure).byBackgroundColor(.clear)
+    public static func detailDisclosure() -> UIButton {
+        UIButton(type: .detailDisclosure).byBackgroundColor(.clear, for: .normal)
     }
 
-    static func infoLight() -> UIButton {
-        UIButton(type: .infoLight).byBackgroundColor(.clear)
+    public static func infoLight() -> UIButton {
+        UIButton(type: .infoLight).byBackgroundColor(.clear, for: .normal)
     }
 
-    static func infoDark() -> UIButton {
-        UIButton(type: .infoDark).byBackgroundColor(.clear)
+    public static func infoDark() -> UIButton {
+        UIButton(type: .infoDark).byBackgroundColor(.clear, for: .normal)
     }
 
-    static func contactAdd() -> UIButton {
-        UIButton(type: .contactAdd).byBackgroundColor(.clear)
+    public static func contactAdd() -> UIButton {
+        UIButton(type: .contactAdd).byBackgroundColor(.clear, for: .normal)
     }
 }
