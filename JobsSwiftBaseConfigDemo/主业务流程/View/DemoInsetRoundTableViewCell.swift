f088c0f8c3f35cfac3d076f4fcb104da68fc38bf
Jobs <lg295060456@gmail.com>
2026-03-15 18:02:08 +0700

    no message

--- JobsSwiftBaseConfigDemo/主业务流程/View/DemoInsetRoundTableViewCell.swift
+++ JobsSwiftBaseConfigDemo/主业务流程/View/DemoInsetRoundTableViewCell.swift
@@ -24,10 +24,7 @@ final class DemoInsetRoundTableViewCell: UITableViewCell,
     // MARK: - Section 圆角配置
     var sectionCornerRadius: CGFloat { 8 }
     // MARK: - Init
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         // MARK: - Base appearance
